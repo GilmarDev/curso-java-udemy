79498ad87e58efc53d01d3728e0f523c2bcc1b8b
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2021-10-12 18:10:09 -0300

    exercícios java udemy

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+/**
+ * 
+ * @author GilmarDev
+ *
+ *@size JDK 1.0
+ */
+public class PrimeiroPrograma {
+
+  public static void main(String[] args) {
+    
+
+    // Sentença de código termina com ;
+    // Mais uma linha...
+    // Fim
+    System.out.println("Primeiro Programa parte #01");
+
+    /*
+     * Linha 1
+     * Linha 2
+     * Linha 3
+     */
+  
+    System.out.println("Primeiro Programa parte #02");
+  }
+}
+
+
