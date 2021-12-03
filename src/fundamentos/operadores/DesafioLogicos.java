028391bdf79dc0fbb517ce9fa7f9644642716e0b
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2021-12-02 22:15:51 -0300

    exercícios java udemy

---
+++ src/fundamentos/operadores/DesafioLogicos.java
@@ -0,0 +1,23 @@
+package fundamentos.operadores;
+
+public class DesafioLogicos {
+
+  public static void main(String[] args) {
+    
+    
+    boolean trabalho1 = false;
+    boolean trabalho2 = false;
+    
+    boolean comprouTV50 = trabalho1 && trabalho2;
+    boolean comprouTV32 = trabalho1 ^ trabalho2;
+    boolean comprouSorvete = trabalho1 || trabalho2;
+    
+    //Operador Unário!
+    boolean maisSaudavel = !comprouSorvete;
+    
+    System.out.println("Comprou TV 50\"? " + comprouTV50);
+    System.out.println("Comprou TV 50\"? " + comprouTV32);
+    System.out.print("Mais saudável ? " + maisSaudavel);
+  }
+
+}
