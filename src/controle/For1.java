b402dde3ae6f68fc3d9c76c08b11834508fc562c
Gilmar Cruz Silva <gilmarcs1@hotmail.com>
2022-01-16 14:32:07 -0300

    exercícios java udemy

---
+++ src/controle/For1.java
@@ -0,0 +1,20 @@
+package controle;
+
+public class For1 {
+  
+  public static void main(String[] args) {
+    
+    
+    for(int contador = 1; contador < 20; contador++) {
+      System.out.println("i %d \n = " + contador);
+      
+    }
+    
+    
+    // exemplo de Laço infinito
+    for(;true;) {
+      System.out.println("Fim");
+    }
+  }
+
+}
